1bb0c31c1a55ea363e043cbda440494caf4ad0ce
haocity <ureygt@gmail.com>
2016-06-01 19:19:38 +0800

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,9 @@
-console.log('This would be the main JS file.');
+
+	changeBgImg();
+	setInterval(changeBgImg,30000);
+	function changeBgImg(){
+	bj="http://haocity.top/img/bj@"+Math.round(Math.random()*2000)+".jpg"
+	 console.log(bj);
+	document.getElementById("body").style.backgroundImage = "url("+bj+")";
+	 }
+
