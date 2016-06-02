c21e27bc3733c4dc05011c35ab2aac31674c9bef
haocity <ureygt@gmail.com>
2016-06-02 11:14:44 +0800

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,17 +1,15 @@
-
-	GetOneImg();  //获取随机图片地址
+GetOneImg();  //获取随机图片地址
 	ChangeOneImg();//进行更换图片操作
-	setInterval(changeBgImg,8000);
-	function GetOneImg(){
+	setInterval(ChangeOneImg,8000);//重复更换图片
+	function GetOneImg(){ 
 	OneImg="http://haocity.top/img/bj@"+Math.round(Math.random()*2000)+".jpg"
-
 	 }
 
 	function ChangeOneImg(){
-	console.log(OneImg);
-	document.getElementById("oneimg").style.backgroundImage = "url("+bj+")";
+	console.log(OneImg);//调试图片地址
+	document.getElementById("oneimg").style.backgroundImage = "url("+OneImg+")";
 	GetOneImg();
-	LoadImg(OneImg);
+	LoadImg(OneImg);//预先加载下一张图片
 	 }
 
 
