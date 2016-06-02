4f1cb8bf4a8c31c3a989d542aaa6717314ef41e3
haocity <ureygt@gmail.com>
2016-06-02 11:10:55 +0800

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,9 +1,21 @@
 
-	changeBgImg();
-	setInterval(changeBgImg,30000);
-	function changeBgImg(){
-	bj="http://haocity.top/img/bj@"+Math.round(Math.random()*2000)+".jpg"
-	 console.log(bj);
-	document.getElementById("oneimg").style.backgroundImage = "url("+bj+")";
+	GetOneImg();  //获取随机图片地址
+	ChangeOneImg();//进行更换图片操作
+	setInterval(changeBgImg,8000);
+	function GetOneImg(){
+	OneImg="http://haocity.top/img/bj@"+Math.round(Math.random()*2000)+".jpg"
+
 	 }
 
+	function ChangeOneImg(){
+	console.log(OneImg);
+	document.getElementById("oneimg").style.backgroundImage = "url("+bj+")";
+	GetOneImg();
+	LoadImg(OneImg);
+	 }
+
+
+    function LoadImg(url) { 
+	var img = new Image(); 
+	img.src = url; 
+	} 
