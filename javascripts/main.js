069d967471483bc4c3bca3f4f012e1b6f4d99436
haocity <ureygt@gmail.com>
2016-06-03 23:35:46 +0800

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,7 +2,7 @@ GetOneImg();  //获取随机图片地址
 	ChangeOneImg();//进行更换图片操作
 	setInterval(ChangeOneImg,8000);//重复更换图片
 	function GetOneImg(){ 
-	OneImg="http://haocity.top/img/bj@"+Math.round(Math.random()*2000)+".jpg"
+	OneImg="https://app.haotown.cn/img/bj@"+Math.round(Math.random()*2000)+".jpg"
 	 }
 
 	function ChangeOneImg(){
