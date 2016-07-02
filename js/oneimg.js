9aab35cc712fabdf661253836360135e9321caf9
疯狂减肥带 <ureygt@gmail.com>
2016-07-02 17:06:12 +0800

    完善js代码 增加菜单

--- js/oneimg.js
+++ js/oneimg.js
@@ -1,19 +1,34 @@
-GetOneImg();  //获取随机图片地址
+	GetOneImg(); //获取随机图片序号
 	ChangerImg();//进行更换图片操作
+	
 	setInterval(ChangerImg,8000);//重复更换图片
+	var OneImg,OneImgnumber;
 	function GetOneImg(){ 
-	OneImg="https://app.haotown.cn/img/bj@"+Math.round(Math.random()*2000)+".jpg"
+	OneImg=Math.round(Math.random() * 2200);
 	 }
 
 	function ChangerImg(){
-	console.log(OneImg);//调试图片地址
-	document.getElementById("oneimg").style.backgroundImage = "url("+OneImg+")";
+	console.log("背景图已更换为"+OneImg);//调试图片
+	OneImgnumber=OneImg;
+	document.getElementById("oneimg").style.backgroundImage = "url(https://app.haotown.cn/img/bj@"+OneImg+".jpg)";
 	GetOneImg();
-	LoadImg(OneImg);//预先加载下一张图片
-	 }
+	LoadImg("https://app.haotown.cn/img/bj@"+OneImg+".jpg");//预先加载下一张图片
+	console.log("开始预加载"+OneImg)	 
+	}
 
 
     function LoadImg(url) { 
 	var img = new Image(); 
 	img.src = url; 
 	} 
+	function showoneimg(){
+        document.getElementById('oneimgmenu').style.display='block';
+        document.getElementById('oneimgmenuimg').style.display='none';
+    }
+    function hideoneimg(){
+        document.getElementById('oneimgmenu').style.display='none';
+        document.getElementById('oneimgmenuimg').style.display='block';
+    }
+    function DownImg(){
+    window.open("https://app.haotown.cn/img.html?img=" +OneImgnumber);
+    }
