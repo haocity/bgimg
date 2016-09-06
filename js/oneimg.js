afb2c9b3f519e360c04bd64fa1d493498ace0888
皓东haocity <ureygt@gmail.com>
2016-09-06 23:09:29 +0800

    首页大改版

--- js/oneimg.js
+++ js/oneimg.js
@@ -1,34 +1,22 @@
+	OneImg=new Object();
+	OneImg.obj=document.getElementById("oneimg");
+	OneImg.down=document.getElementById("downimg");
+	OneImg.join=document.getElementById("join");
 	GetOneImg(); //获取随机图片序号
 	ChangerImg();//进行更换图片操作
-	
 	setInterval(ChangerImg,8000);//重复更换图片
-	var OneImg,OneImgnumber;
 	function GetOneImg(){ 
-	OneImg=Math.round(Math.random() * 2200);
-	 }
-
-	function ChangerImg(){
-	console.log("背景图已更换为"+OneImg);//调试图片
-	OneImgnumber=OneImg;
-	document.getElementById("oneimg").style.backgroundImage = "url(https://app.haotown.cn/img/bj@"+OneImg+".jpg)";
-	GetOneImg();
-	LoadImg("https://app.haotown.cn/img/bj@"+OneImg+".jpg");//预先加载下一张图片
-	console.log("开始预加载"+OneImg)	 
+	OneImg.id=Math.round(Math.random() *4358);
 	}
-
-
+	function ChangerImg(){
+	OneImg.obj.style.backgroundImage = "url(https://t4.haotown.cn/img/bj@"+OneImg.id+".jpg)";
+	OneImg.down.href="https://t4.haotown.cn/img/bj@"+OneImg.id+".jpg";
+	OneImg.join.href="https://t5.haotown.cn/img.html?img="+OneImg.id;
+	GetOneImg();
+	LoadImg("https://app.haotown.cn/img/bj@"+OneImg.id+".jpg");//预先加载下一张图片
+	}
     function LoadImg(url) { 
 	var img = new Image(); 
 	img.src = url; 
 	} 
-	function showoneimg(){
-        document.getElementById('oneimgmenu').style.display='block';
-        document.getElementById('oneimgmenuimg').style.display='none';
-    }
-    function hideoneimg(){
-        document.getElementById('oneimgmenu').style.display='none';
-        document.getElementById('oneimgmenuimg').style.display='block';
-    }
-    function DownImg(){
-    window.open("https://app.haotown.cn/img.html?img=" +OneImgnumber);
-    }
+	
