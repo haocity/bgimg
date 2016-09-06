2c26daa68903362c9c4ab601c6916020bbbab901
皓东haocity <ureygt@gmail.com>
2016-09-06 23:10:05 +0800

    添加傻瓜式接入一图js

---
+++ js/simple.js
@@ -0,0 +1,6 @@
+	OneImg=new Object();
+	OneImg.obj=document.getElementsByTagName('body')[0];
+	OneImg.id=Math.round(Math.random() *4358);
+	OneImg.obj.style.backgroundImage = "url(https://t4.haotown.cn/img/bj@"+OneImg.id+".jpg)";
+	OneImg.obj.style.backgroundSize="cover";
+	OneImg.obj.style.backgroundAttachment="fixed";
